432657d83a6206efd1fc8f49578a1602676016c4
Sajjad <knowledgemarket@gmail.com>
2026-05-07 23:06:03 +0530

    chore: harden SEO meta tags, improve cache-control headers, and expand robots.txt crawler exclusions

--- thank-you.php
+++ thank-you.php
@@ -13,7 +13,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="robots" content="noindex, nofollow">
-
+<meta name="googlebot" content="noarchive, nosnippet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
 
 <style>
